c7b4161e635cd974d174d6efdec317cdd11ab185
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-12-04 21:01:45 +0700

    initial project

---
+++ src/main/java/id/rachman/tabungan/Tabungan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.rachman.tabungan;
+
+/**
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF-1
+ * NIM   : 10119038
+ * Deskripsi Program : program ini berisi class Tabungan
+ */
+
+public class Tabungan {
+    private int saldo;
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah) {
+        return saldo - jumlah;
+    }
+}
---
+++ src/main/java/id/rachman/tabungan/Tester.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.rachman.tabungan;
+
+/**
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF-1
+ * NIM   : 10119038
+ * Deskripsi Program : program ini berisi untuk menampilkan hasil penarikan uang
+ *                     Tabungan dengan konsep pendekatan object oriented
+ */
+
+import java.util.Scanner;
+
+public class Tester {
+    public static int jumlah;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tabungan tabungan = new Tabungan(0);
+        System.out.println("====Program Penarikan Uang====");
+        System.out.println("Masukkan Saldo Awal      : ");tabungan.setSaldo(sc.nextInt());
+        System.out.println("Jumlah uang yang diambil : ");jumlah = sc.nextInt();
+        System.out.println("Saldo Anda Sekarang\t     : " + tabungan.ambilUang(jumlah));
+    }
+}
